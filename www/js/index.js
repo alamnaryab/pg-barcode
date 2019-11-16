0f734d02049a7c2029b5bfc5efd75466beb1bccc
alamnaryab <alamnaryab@ymail.com>
2019-11-16 09:13:54 +0400

    scanner button debugging4

--- www/js/index.js
+++ www/js/index.js
@@ -53,41 +53,7 @@
 // };
 $(function(){ 
     $("#btnScan").click(function(){
-        cordova.plugins.barcodeScanner.scan(
-            function (result) {
-                $('.scanReceiptMsg').html('16');
-                    resetWheel();
-                    $('.scanReceiptMsg').html('18');
-                    document.getElementById('receiptId').value=result.text;
-                    $('.scanReceiptMsg').html('20');
-                    $('.amount').hide();
-                    $('.preloader-wrapper').show();
-                    $('.scanReceiptMsg').hide();
-                    $('.scanReceiptMsg').html('24');
-                    setTimeout(function () {                            
-                        $('.amount').show();
-                        $('.preloader-wrapper').hide();
-                        //$('.scanReceiptMsg').hide();
-                    }, 5000);
-                    $('.scanReceiptMsg').show().html('30');
-            },
-            function (error) {
-                alert("Scanning failed: " + error);
-            },
-            {
-                //preferFrontCamera : true, // iOS and Android
-                showFlipCameraButton : true, // iOS and Android
-                showTorchButton : true, // iOS and Android
-                torchOn: false, // Android, launch with the torch switched on (if available)
-                //saveHistory: true, // Android, save scan history (default false)
-                prompt : "Place a barcode inside the scan area", // Android
-                resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-                //formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-                //orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
-                disableAnimations : true, // iOS
-                disableSuccessBeep: false // iOS and Android
-            }
-         );       
+        $('.scanReceiptMsg').html('56');       
     });
 });
 
